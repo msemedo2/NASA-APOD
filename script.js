13b8a2c21c60bf4b64c06a7f3acb3f94758635c1
unknown <miguel.semedo.2@outlook.com>
2022-10-03 09:14:33 +0100

    Create DOM Elements

--- script.js
+++ script.js
@@ -1,15 +1,73 @@
-const count = 10;
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirm = document.querySelector('.save-confirm');
+const loader = document.querySelector('.loader');
+
+const count = 2;
 const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod/?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+
+// Update DOM
+function updateDOM() {
+	resultsArray.forEach((result) => {
+		// Card container
+		const card = document.createElement('div');
+		card.classList.add('card');
+		// Link
+		const link = document.createElement('a');
+		link.href = result.hdurl;
+		link.title = 'View Full Image';
+		link.target = '_blank';
+		// Image
+		const image = document.createElement('img');
+		image.src = result.url;
+		image.alt = 'NASA Picture of the Day';
+		image.loading = 'lazy';
+		image.classList.add('card-img-top');
+		// Cart Body
+		const cardBody = document.createElement('div');
+		cardBody.classList.add('card-body');
+		// Card Title
+		const title = document.createElement('h5');
+		title.classList.add('card-title');
+		title.textContent = result.title;
+		// Save Text
+		const saveText = document.createElement('p');
+		saveText.classList.add('clickable');
+		saveText.textContent = 'Add to Favorites';
+		// Text
+		const text = document.createElement('p');
+		text.classList.add('card-text');
+		text.textContent = result.explanation;
+		// Copyright Info
+		const footer = document.createElement('small');
+		footer.classList.add('text-muted');
+		// Date
+		const date = document.createElement('strong');
+		date.textContent = result.date;
+		// Copyright
+		const copyrightResult =
+			result.copyright === undefined ? '' : result.copyright;
+		const copyright = document.createElement('span');
+		copyright.textContent = `${copyrightResult}`;
+		// Append
+		footer.append(date, copyright);
+		cardBody.append(title, saveText, text, footer);
+		link.appendChild(image);
+		card.append(link, cardBody);
+		imagesContainer.appendChild(card);
+	});
+}
 
 // Get 10 images from NASA API
 async function getNasaPictures() {
 	try {
 		const response = await fetch(apiUrl);
 		resultsArray = await response.json();
-		console.log(resultsArray);
+		updateDOM();
 	} catch (err) {
 		console.log(err);
 	}
